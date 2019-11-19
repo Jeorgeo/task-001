2adaa1a6139d1dea7a72185d2b7cd89600d788da
Yury_Shakhouski <Jeorgeo@list.ru>
2019-11-19 16:22:38 +0300

    191119-1622

--- task-001/Gifts/Gift.cs
+++ task-001/Gifts/Gift.cs
@@ -1,20 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using task_001.Sweets;
 
 namespace task_001.Gifts
 {
-    public class Gift
+    public class Gift : IEnumerable<Candy>
     {
-        public string NameGift { get; private set; }
+        private List<Candy> candies = new List<Candy>();
 
-        public void WeightGift() { 
-        
-        
+        public uint Weight { get; private set; }
+
+        public void Add(Candy candy)
+        {
+            if (candy == null)
+            {
+                throw new ArgumentNullException(nameof(candy));
+            }
+
+            candies.Add(candy);
+            Weight += candy.TotalWeight;
         }
 
-        public void SortSweets() { }
+  /*      public List<Candy> FindBy(uint minSugarWeight, uint maxSugarWeight)
+        {
+            return candies.Where(candy => candy.SugarWeight >= minSugarWeight && candy.SugarWeight <= maxSugarWeight).ToList();
+        }
 
-        public void AddSweets() { }
+        public void OrderBy<TResult>(Func<Candy, TResult> comparer)
+        {
+            candies = candies.OrderBy(comparer).ToList();
+        }
+
+        public void OrderByDesceding<TResult>(Func<Candy, TResult> comparer)
+        {
+            candies = candies.OrderByDescending(comparer).ToList();
+        }
+*/
+        public IEnumerator<Candy> GetEnumerator()
+        {
+            return candies.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
